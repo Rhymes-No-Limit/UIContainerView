db036ef7da9958779f8d1d6c7db57b17fde0dbc4
Andrey <Rhymes.un.limit@gmail.com>
2025-04-28 14:23:55 +0300

    Add functionality play list

--- UIContainerView/ViewController.swift
+++ UIContainerView/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageCover: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
